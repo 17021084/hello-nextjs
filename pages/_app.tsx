6a50b526c872a706ef447aa68d32639140d35852
Quang Trung <dotrunghg1999@gmail.com>
2021-04-26 21:44:07 +0700

    workwith css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
+import './global.css'
+// just import it in app file and dont need in any page
 
 function MyApp({ Component, pageProps }) {
+  console.log("Hello in app, chay dc tren ca client ")
   return <Component {...pageProps} />
 }
 
---
+++ pages/nicepage.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react'
+import '../styles/Nicepage.module.css'
+export default function Nicepage() {
+  return (
+    <div>
+       <h1 className="red"> Nicepage </h1>
+    </div>
+  );
+}
